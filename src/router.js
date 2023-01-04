8c02b4db664b2fef29e27a9718863083fc32a6a9
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-01-03 19:46:36 -0500

    create routes in app component

--- src/router.js
+++ src/router.js
@@ -1,9 +1,13 @@
 import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"
+import App from "./App"
+import Main from "./pages/main"
+import Currencies from "./pages/currencies"
+import Price from "./pages/price"
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<h1>Hello World</h1>}>
-            
+            <Route path="" element={<Main/>}/>
         </Route>
     )
 )
